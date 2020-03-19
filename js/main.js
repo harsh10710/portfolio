1de19f2c2f5a505b431fbdc8057f71511decef84
harsh10710 <59953079+harsh10710@users.noreply.github.com>
2020-03-19 13:11:17 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//select dom items
+const menubtn=document.querySelector('.menu-btn');
+const menu=document.querySelector('.menu');
+const menubNav=document.querySelector('.menu-nav');
+const menuBranding=document.querySelector('.menu-branding');
+const navItems=document.querySelectorAll('.nv-item');
+
+//set initial stte of menu
+let showmenu=false;
+menubtn.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+    if(!showmenu){
+    menubtn.classList.add('close');
+    menu.classList.add('show'); 
+    menubNav.classList.add('show'); 
+    menuBranding.classList.add('show');
+    navItems.forEach(item=>item.classList.add('show'));
+    showmenu=true;
+    }else{
+
+        menubtn.classList.remove('close');
+        menu.classList.remove('show'); 
+        menubNav.classList.remove('show'); 
+        menuBranding.classList.remove('show');
+        navItems.forEach(item=>item.classList.remove('show'));
+        showmenu=false;
+        
+    }
+}
